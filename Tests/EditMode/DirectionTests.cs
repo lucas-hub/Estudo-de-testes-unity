d0471d07f7dadbb6da88e324ad73564ea5c37cb7
Lucas Marco <54378079+lucas-hub@users.noreply.github.com>
2023-01-30 23:31:31 -0300

    Add files via upload

---
+++ Tests/EditMode/DirectionTests.cs
@@ -0,0 +1,33 @@
+﻿using InfallibleCode;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class DirectionTests
+    {
+        [Test]
+        public void North()
+        {
+            Assert.AreEqual(new Vector3(0, 1, 0), Direction.North.Vector);
+        }
+
+        [Test]
+        public void South()
+        {
+            Assert.AreEqual(new Vector3(0, -1, 0), Direction.South.Vector);
+        }
+
+        [Test]
+        public void West()
+        {
+            Assert.AreEqual(new Vector3(1, 0, 0), Direction.West.Vector);
+        }
+           
+        [Test]
+        public void East_Test()
+        {
+            Assert.AreEqual(new Vector3(-1, 1, 1), Direction.East.Vector);
+        }
+    }
+}
